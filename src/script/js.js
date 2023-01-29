843dd005364fa94f96604919d04c55b1173049d1
estudantedehtml <mauriciopedeo007@gmail.com>
2023-01-28 22:49:21 -0300

    first commit

---
+++ src/script/js.js
@@ -0,0 +1,304 @@
+const addTaskBtn = document.querySelector('#add-task');
+const taskTitleInput = document.querySelector('#task-input');
+const taskList = document.querySelector('#todo-list');
+const subTaskList = document.querySelector('.child-list');
+
+const validations = {
+    checkEmpty: (title) => {
+        if (!title) {
+            throw new Error('Digite algo no campo!')
+        }
+    },
+    checkMinLength: (title) => {
+        if (title.length < 4) {
+            throw new Error('Digite algo válido')
+        }
+    }
+}
+
+const taskActions = {
+    '.add-child': SubTaskCreator,
+    '.confirm-name-btn': SubTasks,
+    'input[type="checkbox"]': CheckBox,
+    '.delete-task': TaskDeleter,
+    '.dropdown-btn': dropdown
+}
+
+const subTaskConfigs = {
+    input: {
+        type: 'text',
+        placeholder: 'Adicione o conteúdo',
+        class: 'task-name-input'
+    },
+    button: {
+        text: 'Adicionar',
+        class: 'confirm-name-btn'
+    },
+    imgDelete: {
+        src: '/src/assets/trash.png',
+        class: 'delete-task'
+    },
+    childList: {
+        class: 'child-list'
+    }
+}
+
+const subTaskElements = {
+    dropdownBtn: {
+        type: 'img',
+        src: '/src/assets/dropdown.png',
+        class: 'dropdown-btn'
+    },
+    checkbox: {
+        type: 'checkbox'
+    },
+    taskDate: {
+        title: '',
+        class: 'date',
+    },
+    taskName: {
+        type: 'text',
+        text: '',
+    },
+    addChildBtn: {
+        type: 'img',
+        src: '/src/assets/add.png',
+        class: 'add-child'
+    },
+    deleteTaskBtn: {
+        type: 'img',
+        src: '/src/assets/trash.png',
+        class: 'delete-task'
+    },
+    childList: {
+        class: 'child-list'
+    }
+}
+
+//Elementos Pais
+function Task() {
+
+    function addTitleTask() {
+        const taskText = taskTitleInput.value;
+
+        validateAndAddTask(taskText)
+        taskTitleInput.value = ''
+    }
+
+    async function validateAndAddTask(title) {
+        const resultValidate = await validateTaskTitle(title)
+
+        if (!resultValidate) {
+            const translateTitle = titleize(title)
+            createElements(translateTitle)
+        }
+    }
+
+    async function createElements(title) {
+        const childTask = document.createElement('li');
+        childTask.classList.add('child-item');
+
+        addTaskToDOM(childTask, title)
+
+        return setItemsLocalStorage()
+    }
+
+    function addTaskToDOM(childTask, title) {
+        const date = getDate();
+
+        childTask.appendChild(createElement('img', subTaskElements.dropdownBtn));
+        childTask.appendChild(createElement('input', subTaskElements.checkbox));
+        childTask.appendChild(createElement('span', subTaskElements.taskName, title));
+        childTask.appendChild(createElement('img', subTaskElements.addChildBtn));
+        childTask.appendChild(createElement('img', subTaskElements.deleteTaskBtn));
+        childTask.appendChild(createElement('span', subTaskElements.taskDate, date));
+        childTask.appendChild(createElement('ul', subTaskElements.childList));
+
+        taskList.appendChild(childTask);
+    }
+
+    addTitleTask()
+}
+
+//Elementos filhos
+function SubTaskCreator(e) {
+
+    function createSubTask(e) {
+        const parentTask = e.target.parentElement;
+        createElements(parentTask);
+    }
+
+    function createElements(parentTask) {
+        const childList = parentTask.querySelector('.child-list');
+        const childTask = document.createElement('li');
+        childTask.classList.add('child-item');
+
+        addTaskToDOM(childTask, childList)
+    }
+
+    function addTaskToDOM(childTask, childList) {
+        childTask.appendChild(createElement('input', subTaskConfigs.input));
+        childTask.appendChild(createElement('button', subTaskConfigs.button));
+        childTask.appendChild(createElement('img', subTaskConfigs.imgDelete));
+        childTask.appendChild(createElement('ul', subTaskConfigs.childList));
+
+        return childList.appendChild(childTask);
+    }
+
+    createSubTask(e)
+}
+
+function SubTasks(e) {
+
+    function confirmSubTaskTitle(e) {
+
+        const parentTask = e.target.parentElement;
+        const taskNameInput = parentTask.querySelector('.task-name-input');
+        const taskName = taskNameInput.value;
+
+        validateSubTaskTitle(taskName, parentTask)
+    }
+
+    async function validateSubTaskTitle(title, parentTask) {
+        const resultValidate = await validateTaskTitle(title)
+
+        if (!resultValidate) {
+            const translateTitle = titleize(title)
+            createElements(parentTask, translateTitle)
+
+            setItemsLocalStorage()
+        }
+    }
+
+    async function createElements(parentTask, title) {
+        const childList = parentTask.querySelector('.child-list');
+        const childTask = document.createElement('li');
+        childTask.classList.add('child-item');
+
+        addTaskToDOM(childTask, childList, title)
+        return setItemsLocalStorage()
+    }
+
+    function addTaskToDOM(childTask, childList, title) {
+        const date = getDate()
+
+        childTask.appendChild(createElement('img', subTaskElements.dropdownBtn));
+        childTask.appendChild(createElement('input', subTaskElements.checkbox));
+        childTask.appendChild(createElement('span', subTaskElements.taskName, title));
+        childTask.appendChild(createElement('img', subTaskElements.addChildBtn));
+        childTask.appendChild(createElement('img', subTaskElements.deleteTaskBtn));
+        childTask.appendChild(createElement('span', subTaskElements.taskDate, date))
+        childTask.appendChild(createElement('ul', subTaskElements.childList));
+
+        return childList.appendChild(childTask)
+    }
+
+    confirmSubTaskTitle(e)
+}
+
+function createElement(type, config, title) {
+    const element = document.createElement(type);
+    if (config.class) element.classList.add(config.class);
+    if (config.src) element.src = config.src;
+    if (config.text) element.textContent = config.text;
+    if (title) element.textContent = title;
+    if (config.onClick) element.onclick = config.onClick;
+    if (config.type) element.type = config.type;
+    if (config.placeholder) element.placeholder = config.placeholder;
+
+    return element;
+}
+
+function getDate() {
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+}
+
+function titleize(text) {
+    let loweredText = text.toLowerCase();
+    let wordsArray = loweredText.split(" ");
+
+    let words = wordsArray.map((word) => {
+        let firstLetter = word[0];
+        if (word.length > 3) {
+            return firstLetter.toUpperCase() + word.slice(1);
+        }
+        if (word.length < 2) {
+            return firstLetter.toUpperCase() + word.slice(1) + '.';
+        }
+
+        return firstLetter + word.slice(1);
+
+    });
+
+    return words.join(" ");
+}
+
+function validateTaskTitle(title) {
+    try {
+        runValidations(title)
+    } catch (err) {
+        return err
+    }
+}
+
+function setItemsLocalStorage() {
+    const teste = taskList.innerHTML
+    return localStorage.setItem('@tasklist:todo-list', JSON.stringify(teste));
+}
+
+
+function dropdown(e) {
+    const childList = e.target.parentElement.querySelector('.child-list');
+    childList.classList.toggle('show-children');
+
+    const children = childList.querySelectorAll('li');
+    for (let i = 0; i < children.length; i++) {
+        children[i].classList.toggle('show-children');
+    }
+
+    const dropdown = e.target.parentElement.querySelector('.dropdown-btn');
+    dropdown.classList.toggle('show-children');
+}
+
+function TaskDeleter(e) {
+    function removeTask(e) {
+        e.target.parentElement.remove();
+        return setItemsLocalStorage()
+    }
+
+    removeTask(e)
+}
+
+function runValidations(title) {
+    Object.keys(validations).forEach((key) => {
+        validations[key](title);
+    });
+}
+
+
+function getItemsLocalStorage() {
+    taskList.innerHTML = JSON.parse(localStorage.getItem('@tasklist:todo-list'));
+}
+
+if (localStorage.getItem("@tasklist:todo-list")) {
+    getItemsLocalStorage()
+}
+
+
+
+addTaskBtn.addEventListener("click", function () {
+    Task()
+});
+
+taskList.addEventListener("click", function (e) {
+    Object.entries(taskActions).forEach(([selector, action]) => {
+        if (e.target.matches(selector)) {
+            action(e);
+        }
+    });
+});
